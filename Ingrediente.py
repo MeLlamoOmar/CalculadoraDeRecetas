1ec6d2664ab62bff61e9ef2a5a65f3f8c2d6bfaf
Omar Bencosme Tio <obencosmetio@gmail.com>
2025-02-21 19:47:21 -0400

    Se agrego atributos de categoria y precio unitario

--- Ingrediente.py
+++ Ingrediente.py
@@ -1,9 +1,25 @@
 class Ingrediente:
-  def __init__(self, nombre, medida, peso, precio):
+  
+  def __init__(self, nombre, medida, peso, precio, categoria='ingrediente'):
     self.__nombreIngrediente = nombre
     self.__nombreMedida = medida
     self.__pesoIngrediente = peso
     self.__precioIngrediente = precio
+    self.__precioUnitario = precio / peso
+    self.__categoria = categoria
+    
+  @property
+  def categoria(self):
+    return self.__categoria
+  
+  @categoria.setter
+  def categoria(self, categoriaNueva):
+    self.__categoria = categoriaNueva
+    return self.__categoria
+    
+  @property
+  def precioUnitario(self):
+    return self.__precioUnitario
 
   @property
   def nombre(self):
@@ -47,4 +63,5 @@ class Ingrediente:
       Medida del ingrediente: {self.__nombreMedida}
       Peso del Ingrediente: {self.__pesoIngrediente}
       Precio del Ingrediente: {self.__precioIngrediente}
+      Precio unitario: {self.__precioIngrediente / self.__pesoIngrediente}
     ''')
